722545f54962aa6814e752b671f7b66eb59ee396
ClausProbando <tab88fn@gmail.com>
2024-09-27 15:03:08 -0600

    Context menu visibility

--- src/components/ContxtMenu.tsx
+++ src/components/ContxtMenu.tsx
@@ -1,11 +1,51 @@
+import { useEffect, useState } from "react";
 import { AudioSVG } from "../svg/AudioSVG";
 import { CopySVG } from "../svg/CopySVG";
 import { DictionarySVG } from "../svg/DictionarySVG";
 import { SearchSVG } from "../svg/SearchSVG";
 
 export function ContxtMenu() {
+    const [position, setPosition] =  useState([0,0])
+    const [visibility, setVisibility] = useState(false)
+
+
+    useEffect(()=>{
+        const moveContextMenu = (e: MouseEvent)=>{
+            e.preventDefault()
+            console.log(e.clientX, e.clientY);
+            setPosition([e.clientX, e.clientY])
+            setVisibility(true)
+            
+        }
+
+        const hideContextMenu = (e: MouseEvent)=>{
+            const eTarget = e.target as HTMLElement
+            
+            console.log(e);
+            
+            if (typeof eTarget.className === "string" && !eTarget.className.includes("contextMenu")) {
+                setVisibility(false)
+            }
+        }
+
+
+
+        document.addEventListener("contextmenu", moveContextMenu)
+        document.addEventListener("click", hideContextMenu)
+
+        return ()=> {
+            document.removeEventListener("click", hideContextMenu)
+            document.removeEventListener("contextmenu", moveContextMenu)
+        }
+
+
+    
+    }, [])
+
+
     return(
-        <section className="contextMenu">
+        
+        visibility && <section style={{left: position[0], top:position[1]}} className="contextMenu">
             <div className="contextMenu_ColorsCont">
                 <button className="contextMenu_color contextMenu_color--first"></button>
                 <button className="contextMenu_color contextMenu_color--second"></button>
--- src/svg/CopySVG.tsx
+++ src/svg/CopySVG.tsx
@@ -1,5 +1,5 @@
 export function CopySVG() {
     return(
-        <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 352.804 352.804" xmlSpace="preserve"><g></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g > <g> <path d="M318.54,57.282h-47.652V15c0-8.284-6.716-15-15-15H34.264c-8.284,0-15,6.716-15,15v265.522c0,8.284,6.716,15,15,15h47.651 v42.281c0,8.284,6.716,15,15,15H318.54c8.284,0,15-6.716,15-15V72.282C333.54,63.998,326.824,57.282,318.54,57.282z M49.264,265.522V30h191.623v27.282H96.916c-8.284,0-15,6.716-15,15v193.24H49.264z M303.54,322.804H111.916V87.282H303.54V322.804 z"></path> </g> </g></svg>
+        <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 352.804 352.804" xmlSpace="preserve"><g></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g > <g> <path d="M318.54,57.282h-47.652V15c0-8.284-6.716-15-15-15H34.264c-8.284,0-15,6.716-15,15v265.522c0,8.284,6.716,15,15,15h47.651 v42.281c0,8.284,6.716,15,15,15H318.54c8.284,0,15-6.716,15-15V72.282C333.54,63.998,326.824,57.282,318.54,57.282z M49.264,265.522V30h191.623v27.282H96.916c-8.284,0-15,6.716-15,15v193.24H49.264z M303.54,322.804H111.916V87.282H303.54V322.804 z"></path> </g> </g></svg>
     )
 }
