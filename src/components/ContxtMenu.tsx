41b96e14ff937552f26dafcbb0604e8b8e6c426e
ClausProbando <tab88fn@gmail.com>
2024-09-27 23:48:10 -0600

    The menu dosnt overflow

--- src/components/ContxtMenu.tsx
+++ src/components/ContxtMenu.tsx
@@ -5,16 +5,17 @@ import { DictionarySVG } from "../svg/DictionarySVG";
 import { SearchSVG } from "../svg/SearchSVG";
 
 export function ContxtMenu() {
-    const [position, setPosition] =  useState([0,0])
+    const [position, setPosition] =  useState({})
     const [visibility, setVisibility] = useState(false)
 
     //HACER QUE SE PUEDAN SUBRAYAR VARIOS PARRAFOS A LA VEZ
     //HACER QUE EL CONTEXT MENU NO SE VAYA FUERA DE LA PANTALLA CUANDO SE HACE CLICK CERCA DE LOS BORDES
 
     useEffect(()=>{
-        const hideContextMenu = (e: MouseEvent)=>{
+        const hideContextMenu = (e: Event)=>{
             const eTarget = e.target as HTMLElement
                         
+            if (e.type === "scroll") setVisibility(false) 
 
             if (typeof eTarget.className === "string" && !eTarget.className.includes("contextMenu")) {
                 setVisibility(false)
@@ -26,14 +27,29 @@ export function ContxtMenu() {
             if (!isSelected) return
 
             e.preventDefault()
-            setPosition([e.clientX, e.clientY])
+
+
+            console.log(e.pageY, e.clientY, e.offsetY, e.y, e.movementY);
+            //MAGIC STRING
+            const menuPosition = {
+                right: e.pageX + 160 > window.innerWidth? 0: "auto",
+                left: e.pageX + 160 > window.innerWidth? "auto": e.pageX,
+                
+                top: e.clientY + 160 > window.innerHeight? "auto": e.clientY,
+                bottom: e.clientY + 160 > window.innerHeight? 0: "auto"
+
+            }
+
+            setPosition(menuPosition)
             setVisibility(true)  
         }
 
 
 
+
         document.addEventListener("click", hideContextMenu)
         document.addEventListener("contextmenu", moveContextMenu)
+        document.addEventListener("scroll", hideContextMenu)
 
         return ()=> {
             document.removeEventListener("click", hideContextMenu)
@@ -67,7 +83,7 @@ export function ContxtMenu() {
     return(
         
         visibility && 
-        <section style={{left: position[0], top:position[1]}} className="contextMenu">
+        <section style={{...position}} className="contextMenu">
             <div className="contextMenu_ColorsCont">
                 <button onClick={(e)=> highilightColor(e)} className="contextMenu_color contextMenu_color--first"></button>
                 <button onClick={(e)=> highilightColor(e)} className="contextMenu_color contextMenu_color--second"></button>
